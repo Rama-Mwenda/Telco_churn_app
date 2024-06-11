6b2157df257d50d696e02b31d0545cb633de3e97
creative_parasite <nyawijuanki.gmail.com>
2024-06-11 16:00:14 +0300

    Update commit

--- pages/02_📊_Dashboard.py
+++ pages/02_📊_Dashboard.py
@@ -6,4 +6,3 @@ import matplotlib.pyplot as plt
 st.set_page_config(page_title="Dashboard Page", page_icon="📊", layout="wide")
 
 st.title("Dashboard visualizations")
-df = pd.read_csv("Datafiles/new_data.csv")
