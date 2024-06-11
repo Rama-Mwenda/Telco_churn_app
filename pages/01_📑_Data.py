390c2ddb7c53672511dea6847f86c0f08491e617
creative_parasite <nyawijuanki.gmail.com>
2024-06-11 09:23:21 +0300

    Update data page

--- pages/01_📑_Data.py
+++ pages/01_📑_Data.py
@@ -7,9 +7,10 @@ from Utils.features import markdown_table
 st.set_page_config(page_title="Data Page", page_icon="📑", layout="wide")
 st.header("DATA PREVIEW")
 
-col1, col2 = st.columns(spec=[3,3])
+
+col1, col2 = st.columns(spec=[4,3])
 with col1:
-    container = st.container(height=500,border=False)
+    container = st.container(height=300,border=False)
     with container:
         st.markdown("<h3 style='text-align: center;'>FEATURE NAMES 🏷️</h3>", unsafe_allow_html=True)
         st.markdown(""" 
@@ -17,6 +18,11 @@ with col1:
             """)
         with st.expander("Feature names"):
             st.write(markdown_table)
+
+with col2:
+    pass
+
+
 ## create a connection to a database
 @st.cache_resource(show_spinner="connecting to database...")
 def init_connection():
@@ -39,37 +45,78 @@ def running_query(query):
         columns = [column[0] for column in cursor.description]
         df = pd.DataFrame.from_records(rows, columns=columns)
     return df
- 
+
+
 def get_all_columns():
     sql_query = "SELECT * FROM dbo.LP2_Telco_churn_first_3000"
     df = running_query(sql_query)
     return df
+
+
 ## load dataset
 data1= get_all_columns()
 data2 = pd.read_csv("Datafiles\LP2_Telco-churn-second-2000.csv")
 df= pd.concat([data1, data2], axis=0)
 
+
 ## Identify columns in dataset
 categorical_columns = df.select_dtypes(include=["object"]).columns.tolist()
 numerical_columns = df.select_dtypes(include= ["number"]).columns.tolist()
 
-## Create a select box
-with col2:
-    st.markdown("""
-        <style>
-    .stSelectbox > div[data-baseweb="select"] > div {
-         width: 500px; /* adjust the width here */
-     }
-     </style>
-    """, unsafe_allow_html=True)
-    container = st.container(height=500, border=False)  # adjust the width here
-    with container:
-        st.markdown("<h2 style='text-align: center;'>DATASET🛢️ </h2>", unsafe_allow_html=True)
-        options= st.selectbox("Select", ('all columns', 'numerical_columns', 'categorical_columns'))
-        if options == "all columns":
-            st.write(df)
-        elif options == "categorical_columns":
-            st.write(df[categorical_columns])
-        elif options == "numerical_columns":
-            st.write(df[numerical_columns])
 
+## Create a select box
+container= st.container(height=500,border=False)
+with container:
+    col1, col2 = st.columns(spec=[5,4])
+
+    with col1:
+        st.markdown("""
+        <style>
+        .stSelectbox > div[data-baseweb="select"] > div {
+            width: 500px; /* adjust the width here */
+            }
+        </style>
+            """, unsafe_allow_html=True)
+        container = st.container(height=500, border=False)  # adjust the width here
+        with container:
+            st.markdown("<h2 style='text-align: center;'>DATASET🛢️ </h2>", unsafe_allow_html=True)
+            options= st.selectbox("Select", ('all columns', 'numerical_columns', 'categorical_columns'))
+            if options == "all columns":
+                st.write(df)
+            elif options == "categorical_columns":
+                st.write(df[categorical_columns])
+            elif options == "numerical_columns":
+                st.write(df[numerical_columns])
+
+
+
+    with col2:
+    
+        st.markdown("""
+        <style>
+        .stSelectbox > div[data-baseweb="select"] > div {
+        width: 300px; /* adjust the width here */
+        }
+        </style>
+        """, unsafe_allow_html=True)
+    
+        service_attributes=df[['PhoneService', 'MultipleLines', 'InternetService','OnlineSecurity',
+                    'OnlineBackup', 'DeviceProtection','TechSupport'
+                    ]]
+    
+        contracts=df[["Contract", "PaperlessBilling","PaymentMethod"]]
+    
+        personal_attributes=df[["gender", "Partner", "Dependents"]]
+    
+        container= st.container(height=500, border=False)
+    
+        with container:
+            st.markdown("<h3 style='text-align: center;'>ATTRIBUTES✅</h3>", unsafe_allow_html=True)
+            choices= st.selectbox("Select feature attributes",("service_attributes","contracts","personal_attributes"))
+            if choices == "personal_attributes":
+                st.write(personal_attributes)
+            elif choices == "contracts":
+                st.write(contracts)
+            else:
+                st.write(service_attributes)
+    
