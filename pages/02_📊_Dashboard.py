32e05343d35ab9338314a3b81176934be67bcc10
creative_parasite <nyawijuanki.gmail.com>
2024-06-15 15:56:47 +0300

    Update dashboard

--- pages/02_📊_Dashboard.py
+++ pages/02_📊_Dashboard.py
@@ -6,18 +6,19 @@ import plotly.express as px
 import plotly.figure_factory as ff
 from Utils.authenticator import app
 
-st.set_page_config(page_title="Dashboard Page", 
-                   page_icon="📊", 
-                   layout="wide"
-                   )
+st.set_page_config(
+    page_title="Dashboard Page", 
+    page_icon="📊", 
+    layout="wide"
+    )
 
 
-col1, col2= st.columns(2)
-with col1:
-    app()
+# col1, col2= st.columns(2)
+# with col1:
+#     app()
     
-with col2:
-    pass
+# with col2:
+#     pass
     
 def load_data():
     df= pd.read_csv("Datafiles/new_data.csv")
@@ -25,38 +26,50 @@ def load_data():
 
 df = load_data()
 
-col1, col2 = st.columns(2)
-with col1:
-    pass
-with col2:
-    st.selectbox("Choose your visualizations", ["EDA", "KPIs"])
-
-##KPIs
-st.markdown("KEY METRICS")
-Kpi1, Kpi2 = st.columns(2)
-Kpi1.metric("Average_tenure", value= 32, delta=-24)
-Kpi2.metric("Average_monthly_charges", value= 65.09, delta= +18.0)
-
-
-chart1, chart2 = st.columns(spec=[5,3])
-with chart1:
+def eda_dashboard():
+    st.markdown("#### EXPLORATORY DASHBOARD")
+    
+    chart1, chart2 = st.columns(spec=[5,3])
+    with chart1:
     ## plot a histogram
-    fig= px.histogram(df, x=['tenure', 'monthlycharges'])
-    st.plotly_chart(fig)
+        fig= px.histogram(df, x=['tenure', 'monthlycharges'], title="Univariate analysis")
+        st.plotly_chart(fig)
+        
+    with chart2:
+        ## plot a barchart
+        bar = px.bar(df, x="churn")
+        st.plotly_chart(bar)
+    
+    
 
-    ## plot a barchart
-    bar = px.bar(df, x="churn")
-    st.plotly_chart(bar)
 
+def kpi_dashboard():
+    st.markdown("### KEY PERFORMANCE INDICATORS")
+    
+    st.markdown("KEY METRICS")
+    Kpi1, Kpi2 = st.columns(2)
+    Kpi1.metric("Average_tenure", value= 32, delta=-24)
+    Kpi2.metric("Average_monthly_charges", value= 65.09, delta= +18.0)
+    
     ## plot gender bar chart
     chart=px.bar(df, x="gender", y="monthlycharges", color="churn")
     st.plotly_chart(chart)
-    # ## plot a pie chart
-    # sizes= df["churn"]
-    # labels= df["gender"]
-    
-    # fig1, ax1 = plt.subplots()
-    # ax1.pie(sizes,labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
-    # ax1.axis('equal')
-    # pie = px.pie(df, values="churn", names="gender")
-    # st.pyplot(pie) 
+
+
+
+
+
+
+if __name__ == "__main__":
+    st.title("Dashboard")
+    col1, col2= st.columns(2)
+    with col1:
+        pass
+    with col2:
+        st.selectbox("Choose your visualizations",options= ["EDA", "KPIs"], key="slected_dashboard_type")
+        
+    if st.session_state["slected_dashboard_type"] == "EDA":
+        eda_dashboard()
+    else:
+        kpi_dashboard()
+        
