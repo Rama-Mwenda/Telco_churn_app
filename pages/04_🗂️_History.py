503646a77ed928864d014eed97c7269c313acb87
creative_parasite <nyawijuanki.gmail.com>
2024-06-21 13:52:48 +0300

    Add new history file data

--- pages/04_🗂️_History.py
+++ pages/04_🗂️_History.py
@@ -1,3 +1,20 @@
-import streamlit as st 
+import streamlit as st
+import os
+import pandas as pd 
 
 st.set_page_config(page_title="History_page", page_icon="🗂️", layout="wide")
+
+
+def display_history():
+    csv_path = "Datafiles/history.csv"
+    csv_exists = os.path.exists(csv_path)
+    
+    if csv_exists:
+        history = pd.read_csv(csv_path)
+        st.dataframe(history)
+        
+        
+if __name__ == "__main__":
+    st.title("History page")
+    display_history()
+        
