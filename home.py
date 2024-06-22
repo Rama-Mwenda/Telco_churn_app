10968d959d93b40ccb2083ad89ddf6e28753d2db
creative_parasite <nyawijuanki.gmail.com>
2024-06-22 23:18:48 +0300

    Update home

--- home.py
+++ home.py
@@ -78,5 +78,5 @@ If you have any enquiries, feel free to reach out to us:
     st.link_button("LINKED IN", 'www.linkedin.com/in/evalyne-kamuri')
     lIVE_DEMO = st.button("LIVE DEMO")
     if lIVE_DEMO:
-        media = open("C:/Users/ADMIN/Downloads/CHURN CIPHER.mp4", "rb")
+        media = open("assets/CHURN CIPHER.mp4", "rb")
         st.video(media,format="mp4")
